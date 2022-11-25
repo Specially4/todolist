d48b28217f5dd1aded73b9c64270ba663d56a0fe
Specially4 <kotenkovsn@gmail.com>
2022-11-25 14:13:31 +0700

    Добавил авторизацию. Попробовал переопределить метод post

--- core/views.py
+++ core/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
-from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, GenericAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
 from core.models import User
-from core.serializers import UserCreateSerializer, RetrieveUserSerializer
+from core.serializers import (
+    UserCreateSerializer,
+    RetrieveUserSerializer,
+    LoginSerializer
+)
 
 
 class CreateUserView(CreateAPIView):
@@ -18,3 +23,14 @@ class RetrieveUserView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = RetrieveUserSerializer
     permission_classes = [IsAuthenticated]
+
+
+class LoginUserView(GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer: LoginSerializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        user_serializer = RetrieveUserSerializer(instance=user)
+        return Response(user_serializer.data)
